37a89431b2a2a4961704d824db35891207f10997
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-02-22 15:47:23 -0600

    first

---
+++ 12_switch.js
@@ -0,0 +1,18 @@
+//switch
+
+var color = prompt("What's your favorite primary color?","Type your favorite color here");
+
+switch(color) {
+  case 'red':
+    console.log("Red's a good color!");
+    break;
+  case 'blue':
+    console.log("That's my favorite color, too!");
+    break; 
+  case 'yellow':
+    console.log("That's the shiniest color of all!");
+    break;
+    
+  default:
+    console.log("I don't think that's a primary color!");
+}
